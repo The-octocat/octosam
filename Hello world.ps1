2f33c5389fef8d55413907de9415a96f8030825e
Fortino Samuel <67204076+samuel-velazquez@users.noreply.github.com>
2020-11-13 10:12:52 -0700

    Add files via upload
    Track is document and set address so replace in bot sheet run

---
+++ Hello world.ps1
@@ -0,0 +1,70 @@
+# Hello world
+so "in" this = do {
+    
+} until (condition) [last = [ArgumentCompleter({
+    [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+    param(
+        [string] $CommandName,
+        [string] $ParameterName,
+        [string] $WordToComplete,
+        [System.Management.Automation.Language.CommandAst] $CommandAst,
+        [System.Collections.IDictionary] $FakeBoundParameters
+    )
+    
+    OutputType = #Requires -ShellId shell-id git : repo def ´samuel-velazquez´ class path = <path> or component #endregion
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [TypeName]
+        $ParameterName
+    )
+})]]
+{
+    return -- select it true = http-equiv and update is = script 
+    ?xml = type and them add cell  # %% [markdown] tasks {
+        imports = element import - print ("Hello world") = set 
+        class end or gettting = delete are model do async shell/html/javascript or dbtype = #region 
+        # if param name app name # %% address string = path
+    }
+}
+ [DscResource()]
+ class NameOfResource {
+     [DscProperty(Key)]
+     [string] $KeyName
+     
+     # Gets the resource's current state.
+     [NameOfResource] Get() {
+         
+         return $this
+     }
+     
+     # Sets the desired state of the resource.
+     [void] Set() {
+         
+     }
+     
+     # Tests if the resource is in the desired state.
+     [bool] Test() {
+         
+     }
+ }
+ # source from [script] for (){
+     schtasks.exe
+ }
+
+ # Family FRONT PAGE (){
+ [@{name='PropertyName';expression={$_.PropertyValue}}]
+ 
+}
+
+# Called like them or basic left draft ()
+{
+    dasHost.exe [[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ParamName')]]
+}
+
+# Set and jet in prototype drawing ()
+{
+    [param] ´samuel-velazquez´ '.\From Bug' inlineScript { ib_nuget.exe
+        
+    }
+}
